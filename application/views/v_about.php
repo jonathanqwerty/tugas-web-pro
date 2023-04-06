27ac6fbb4de1942557505c36ebe0eeccbd7ffa59
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-04-06 23:19:56 +0700

    update

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Imam</li>
-        <li>Nama Belakang : Nawawi</li>
+        <li>Nama Depan : Jonathan</li>
+        <li>Nama Belakang : Nathanael</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+        <li> Jl.Thalib II No.8 Jakarta Barat</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
