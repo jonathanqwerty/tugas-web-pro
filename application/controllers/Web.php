f144c28af221b2166f3f5e67c18edab3f912c48c
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-05-04 14:55:27 +0700

    update

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -32,16 +32,55 @@ class Web extends CI_Controller
         $this->load->view('web/templates/v_footer', $data);
     }
 
+
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
-        $data['judul'] = "Hasil Form";
-        $this->load->view('web/templates/v_header', $data);
-        $this->load->view('web/v_cetak', $data);
-        $this->load->view('web/templates/v_footer', $data);
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode MTK',
+            'required|numeric',
+            array(
+                'required' => 'Harap isi Kode MTK!',
+                'numeric' =>  'Kode MTK hanya boleh mengandung angka!'
+            )
+        );
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama MTK',
+            'required|min_length[3]',
+            array(
+                'required' => 'Harap isi Nama MTK!',
+                'min_length' => 'Nama MTK Tidak Boleh Kurang dari 3 Karakter!'
+            )
+        );
+        $this->form_validation->set_rules(
+            'sks',
+            'SKS',
+            'required',
+            array(
+                'required' => 'Harap pilih SKS!'
+            )
+        );
+
+        if ($this->form_validation->run() == FALSE) {
+            // if validation fails, show form again with validation errors
+            $data['judul'] = "Hasil Form";
+            $this->load->view('web/templates/v_header', $data);
+            $this->load->view('web/v_form');
+            $this->load->view('web/templates/v_footer', $data);
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $data['judul'] = "Hasil Form";
+            $this->load->view('web/templates/v_header', $data);
+            $this->load->view('web/v_cetak', $data);
+            $this->load->view('web/templates/v_footer', $data);
+        }
     }
 }
